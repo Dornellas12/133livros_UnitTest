adc87ef9558ec0f90a17971bed9c3fc29d4b0fc3
Brunno Dornelas <dornellas_12@hotmail.com>
2022-04-11 20:26:11 -0300

    Testes de unidade utilizando o Framework Pytest.

--- main.py
+++ main.py
@@ -10,10 +10,32 @@ def somar_dois_numeros(numero1, numero2):
 def multiplicar_dois_numeros(numero1, numero2):
     return numero1 * numero2
 
+
 def subtrair_dois_numeros(numero1, numero2):
     return numero1 - numero2
 
-if __name__ == '__main__': # Code Trigger
+
+def dividir_dois_numeros(numero1, numero2):
+    try:   # Tente Fazer
+        return numero1 / numero2
+    except ZeroDivisionError:
+        return 'divisao por zero'    # Tratamento de erro
+
+
+def calcular_area_quadrado(lado):
+    return lado*lado
+
+
+def calcular_area_retangulo(largura, comprimento):
+    return comprimento*largura
+
+
+def calcular_area_triangulo(largura, comprimento):
+    return (comprimento * largura) / 2
+
+
+if __name__ == '__main__':  # Code Trigger
+
     dizer_oi('Brunno')
     # soma dos numeros
     numero1 = 2
@@ -22,11 +44,15 @@ if __name__ == '__main__': # Code Trigger
     print(f'\nA soma de {numero1} e {numero2} é igual a {resultado}')
 
 
-    # multiplicar dois numeros
+# multiplicar dois numeros
     resultado = multiplicar_dois_numeros(3, 5)
     print(f'\nO resultado da multiplicação é {resultado}')
 
 
-    # subtrair dois numeros
+# subtrair dois numeros
     resultado = subtrair_dois_numeros(100, 50)
     print(f'\nO resultado da subtração é {resultado}')
+
+# dividir dois numeros
+    resultado = dividir_dois_numeros(100, 50)
+    print(f'\nO resultado da divisão é {resultado}')
--- pytest_contas.py
+++ pytest_contas.py
@@ -1,19 +1,88 @@
 import pytest
 import main
+import csv
 
 lista_para_multiplicar = [
-        (2, 3, 6),
-        (0, 4, 0),
-        (-5, -3, 15),
-        (8, 1000, 8000)
+    (2, 3, 6),
+    (0, 4, 0),
+    (-5, -3, 15),
+    (8, 1000, 8000)
 ]
-
 
 @pytest.mark.parametrize('numero1, numero2, resultado_esperado', lista_para_multiplicar)
 def testmultiplicar_dois_numeros(numero1, numero2, resultado_esperado):
     # Configura - vira da lista
     # Executa
     resultado_obtido = main.multiplicar_dois_numeros(numero1, numero2)
+
+    # Valida
+    assert resultado_esperado == resultado_obtido
+
+
+# Exemplo de operação por CSV.
+def ler_dados_csv():
+    dados_csv = []  # criamos uma lista vazia
+    nome_arquivo = 'C:\\Users\\Particular\\PycharmProjects\\133livros\\vendors\\massa_dividir_dois_numeros.csv'
+    try:
+        with open(nome_arquivo, newline='') as arquivo_csv:
+            campos = csv.reader(arquivo_csv, delimiter=';')
+            next(campos)
+            for linha in campos:
+                dados_csv.append(linha)
+        return dados_csv
+    except FileNotFoundError:
+        print(f'Arquivo não encontrado: {nome_arquivo}')
+    except Exception as fail:
+        print(f'Falha não prevista: {fail}')
+
+
+@pytest.mark.parametrize('id, numero1, numero2, resultado_esperado, tipo_teste', ler_dados_csv())  # Ler Arquivo CSV
+def testdividir_dois_numeros(id, numero1, numero2, resultado_esperado, tipo_teste):  # Trigger para rodar teste
+
+    # Executa
+    resultado_obtido = main.dividir_dois_numeros(int(numero1), int(numero2))
+
+    # Valida
+    if tipo_teste == 'positivo':
+        assert float(resultado_esperado) == resultado_obtido
+    else:
+        assert resultado_esperado == resultado_obtido
+
+# TDD
+
+def teste_calcular_area_quadrado():
+    # Configura/Prepara
+    lado = 3
+    resultado_esperado = 9
+
+    # Executa
+    resultado_obtido = main.calcular_area_quadrado(lado)
+
+    # Valida
+    assert resultado_esperado == resultado_obtido
+
+
+def teste_calcular_area_retangulo():
+    # Configura/Prepara
+    largura = 5
+    comprimento = 4
+    resultado_esperado = 20
+
+    # Executa
+    resultado_obtido = main.calcular_area_retangulo(largura, comprimento)
+
+    # Valida
+    assert resultado_esperado == resultado_obtido
+
+
+def teste_calcular_area_triangulo():
+    # Configura/Prepara
+    largura = 10
+    comprimento = 5
+    resultado_esperado = 25
+
+    # Executa
+    resultado_obtido = main.calcular_area_triangulo(largura, comprimento)
 
     # Valida
     assert resultado_esperado == resultado_obtido
